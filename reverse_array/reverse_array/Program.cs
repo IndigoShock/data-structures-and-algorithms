2bfe285b554c161ff6c24ef28c44d92a27345f89
stephendeanharper <stephendeanharper@gmail.com>
2018-06-04 19:41:48 -0700

    Initial commit

---
+++ reverse_array/reverse_array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace array_reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayReverse();
+        }
+
+        public static void ArrayReverse()
+        {
+            int[] GivenArray = new int[] { 3, 2, 1 };
+            int[] ReverseArray = new int[GivenArray.Length];
+
+            for (var i = GivenArray.Length - 1; i >= 0; i--)
+            {
+                ReverseArray[i] = GivenArray[i];
+                Console.WriteLine(ReverseArray[i]);
+            }
+        }
+    }
+}
