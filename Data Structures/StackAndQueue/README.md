7957092045036374a893e9cee815de9ab6547ecc
stephendeanharper <stephendeanharper@gmail.com>
2018-06-18 21:46:00 -0700

    Updated README

---
+++ Data Structures/StackAndQueue/README.md
@@ -0,0 +1,19 @@
+## Stacks and Queues ##
+
+1. Visuals of what it looks like
+![Image](asset/StacksAndQueuesImplementation.jpg)
+
+2. How you implemented it 
+For stacks, the nodes sit on top. The functionality of it is only two methods: pushing and popping. If pushed, a node goes in. If popped, a node comes out. Only one way in and one way out. First in, first out.
+For queues, the nodes sit in a line. The functionality are two methods: queueing and dequeueing. If queued, the node goes into the front. If dequeued, a node comes out from the rear. First in, last out.
+
+3. What type of uses you would use this for (provide 2 examples)
+   a. "Calculators employing reverse Polish notation use a stack structure to hold values." 1*
+   b. "Dynamic Memory Allocation" 2*
+
+Acknowledgements
+Instructor - Amanda Iverson
+TA's - Philip Werner, Erik Plyushko
+Classmates - Jimmy Chang for helping with unit testing. Jackie Ly for the whiteboard image.
+1* = https://en.wikibooks.org/wiki/Data_Structures/Stacks_and_Queues
+2* = https://www.sanfoundry.com/c-programming-examples-stacks/
--- Data Structures/StackAndQueue/StackAndQueue/Program.cs
+++ Data Structures/StackAndQueue/StackAndQueue/Program.cs
@@ -10,8 +10,13 @@ namespace StackAndQueue
             Console.WriteLine("Hello World!");
 
             InstantiateStack();
+
+            InstantiateQueue();
         }
 
+        /// <summary>
+        /// This method instantiates the stack and states the peek, top values then pops and shows new top value.
+        /// </summary>
         public static void InstantiateStack()
         {
             Stack stack = new Stack(new Node(1));
@@ -21,12 +26,16 @@ namespace StackAndQueue
             stack.Push(new Node(8));
             stack.Push(new Node(9));
 
-            Console.WriteLine(stack.Peek().Value);
-            Console.ReadLine();
-            Console.WriteLine(stack.Pop());
+            Console.WriteLine($"The Peek is {stack.Peek().Value}");
+            Console.WriteLine($"The top is {stack.Top.Value}");
+            stack.Pop();
+            Console.WriteLine($"After popping, the new top is {stack.Top.Value}");
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// This method instantiates the queue and states the peek, and front values, then dequeues and shows new front value.
+        /// </summary>
         public static void InstantiateQueue()
         {
             Queue queue = new Queue(new Node(1));
@@ -36,9 +45,10 @@ namespace StackAndQueue
             queue.Enqueue(new Node(8));
             queue.Enqueue(new Node(9));
 
-            Console.WriteLine(queue.Peek().Value);
-            Console.ReadLine();
-            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine($"The Peek for this queue is {queue.Peek().Value}");
+            Console.WriteLine($"The front value is {queue.Front.Value}");
+            queue.Dequeue();
+            Console.WriteLine($"When dequeued, the new front value is {queue.Front.Value}");
             Console.ReadLine();
         }
     }
