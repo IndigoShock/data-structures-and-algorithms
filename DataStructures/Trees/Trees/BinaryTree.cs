aeced0d3027e1a887459990b64937848132bcdc1
stephendeanharper <stephendeanharper@gmail.com>
2018-06-28 20:30:02 -0700

    updated method comments for binary tree and binary search tree

--- DataStructures/Trees/Trees/BinaryTree.cs
+++ DataStructures/Trees/Trees/BinaryTree.cs
@@ -5,6 +5,7 @@ namespace Trees
 {
     public class BinaryTree
     {
+        //this traverses through the BST by preorder: root, left, right
         public void PreOrder(Node node)
         {
             Console.WriteLine(node.Value);
@@ -20,6 +21,7 @@ namespace Trees
             }
         }
 
+        //this traverses through the BST in order: left, root, right
         public void InOrder(Node node)
         {
             if (node.LeftChild != null)
@@ -35,6 +37,7 @@ namespace Trees
             }
         }
 
+        //this traverses through the BST by postorder: left, right, root.
         public void PostOrder(Node node)
         {
             if (node.LeftChild != null)
@@ -49,6 +52,8 @@ namespace Trees
 
             Console.WriteLine(node.Value);
         }
+
+        //this method goes through the BST via breadth first. this checking as if it were a S shaped-squiggle rather than depth first. this queues up all of the nodes in the BST and enqueues them. first with the trypeek and will come out with the root. it will check the front and dequeue, writing the value. then check if any of the next nodes are not null. if it is, it will enqueue.
 
         public void BreadthFirst(Node root)
         {
@@ -70,6 +75,7 @@ namespace Trees
             }
         }
 
+        //the search on this method will be looking through for the left and right child and return a boolean. if the value is found, it will return true. if not found, then return false.
         public bool Search(Node root, int value)
         {
             Queue<Node> breadth = new Queue<Node>();
