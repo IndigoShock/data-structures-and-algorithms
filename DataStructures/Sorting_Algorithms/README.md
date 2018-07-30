84dc06b86b09a28cedda58b7e47c9e4b7d190023
stephendeanharper <stephendeanharper@gmail.com>
2018-07-30 11:46:33 -0700

    completed sorting algorithm (insertion sort). updated readme with visual and tests

---
+++ DataStructures/Sorting_Algorithms/README.md
@@ -0,0 +1,16 @@
+# Sorting Algorithms (Insertion Sort)
+
+1. Visuals of what it looks like:
+![Image](asset/SortingAlgorithm.jpg)
+
+2. How you implemented it 
+Sorting Algorithms are data structures. It intakes an array of values and moves the values around based whether the current value is larger or smaller than the adjacent value. If it is or not, the current value will move where appropriate. If it is larger, it will move to the right. If it is smaller, it will move to the left. Once completed, it will stop and return the array as sorted.
+
+3. What type of uses you would use this for (provide 2 examples)
+	a. "Efficient for (quite) small data sets, much like other quadratic sorting algorithms"
+	b. "Efficient for data sets that are already substantially sorted"
+
+[Instructor](Amanda Iverson)
+[TA's](Philip Werner, Erik Plyushko, Jeff Martinez)
+[Classmates](https://github.com/jcqnly)
+[Reference for Uses](https://en.wikipedia.org/wiki/Insertion_sort)
---
+++ DataStructures/Sorting_Algorithms/Sorting_Algorithms/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Sorting_Algorithms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] unsortedArray = new int[] { 1, 5, 4, 3 };
+
+            InsertionSort(unsortedArray);
+            Console.ReadLine();
+        }
+
+        public static int[] InsertionSort(int[] unsorted)
+        {
+            int[] A = unsorted;
+
+            foreach (int numbers in unsorted)
+            {
+                Console.WriteLine($"This is the unsorted array: {numbers}");
+            }
+
+
+            int i = 1;
+            while (i < A.Length)
+            {
+                int x = A[i];
+                int j = i - 1;
+
+                while (j >= 0 && A[j] > x)
+                {
+                    A[j + 1] = A[j];
+                    j = j - 1;
+                }
+
+                A[j + 1] = x;
+                i = i + 1;
+            }
+            foreach (int moreNumbers in unsorted)
+            {
+                Console.WriteLine($"And this is now sorted: {moreNumbers}");
+            }
+            return unsorted;
+        }
+    }
+}
